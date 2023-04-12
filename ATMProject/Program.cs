590a8f6f493de602c93cc0039ff55b69be400cde
Mohammad Amin Shiravi <aminmamad98@gmail.com>
2023-04-12 19:55:47 +0430

    Add project files.

---
+++ ATMProject/Program.cs
@@ -0,0 +1,42 @@
+﻿using ATMProject.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATMProject
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine();
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.Write("-------------");
+			Console.Write("MOHAMMAD ATM");
+			Console.Write("-------------");
+			Console.WriteLine();
+			Console.ForegroundColor = ConsoleColor.Blue;
+			Console.WriteLine("Welcome To My ATM");
+			bool isCardCorrect = false;
+			while (!isCardCorrect)
+			{
+				Console.WriteLine("PLEASE ENTER YOU CRAD NUMBER!!");
+				string cardNumberStr = Console.ReadLine();
+				long cardNumber = Utility.StringToLong(cardNumberStr);
+				Console.WriteLine($"\n{cardNumber} Is it Your Card? Press 1 for Yes, 2 for No!!");
+				string isItMyCard = Console.ReadLine();
+				if (isItMyCard == "1")
+				{
+					isCardCorrect = true;
+				}
+				else if (isItMyCard != "2")
+				{
+					Console.WriteLine("Invalid input. Please try again.");
+				}
+			}
+
+		}
+	}
+}
---
+++ ATMProject/UI/Utility.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATMProject.UI
+{
+	public class Utility
+	{
+		public static long StringToLong(string str)
+		{
+			if (long.TryParse(str, out long result))
+			{
+				return result;
+			}
+			else
+			{
+				throw new ArgumentException("Invalid input. The input string must be a valid long number.");
+			}
+		}
+	}
+}
